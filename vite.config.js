59764ada4ccb3fdd8fffad08296168e8b5578047
Thomas Cline <149266783+tdcline1@users.noreply.github.com>
2025-01-05 16:24:05 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-        base: "/React-Final-Project-Practic",
+        base: "/React-Final-Project-Practice",
     plugins: [react()],
 })
